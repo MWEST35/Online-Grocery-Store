0b5b32b0c51716aa5af0af8febd43608d2e49c81
Philbert5600 <Philbert5600@gmail.com>
2024-04-29 04:32:01 -0500

    CartEngine logic for total calculation and tests.

--- Engines/CartEngine.cs
+++ Engines/CartEngine.cs
@@ -1,20 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Engines
 {
-    internal class CartEngine : ICartEngine
+    public class CartEngine
     {
-        double ICartEngine.calculateItemCost(float salePct, int units, double pricePerUnit)
+
+        public double calculateItemCost(double salePct, int units, double pricePerUnit)
         {
             double saleApplied = pricePerUnit * (1 - salePct);
-            return (salePct * units);
+            return Math.Round((saleApplied * units), 2);
         }
 
-        double ICartEngine.calculateTotal(List<double> itemCosts)
+        public double calculateTotal(List<double> itemCosts)
         {
             double gross = 0;
             for (int i = 0; i < itemCosts.Count; i++)
@@ -24,10 +26,171 @@ namespace Engines
             return gross;
         }
 
-        double ICartEngine.totalWithTax(double gross)
+        public double calculateTotalWithTax(double gross, string state = "")
         {
-            // Assumes Nebraska sales tax
-            return (gross + (gross * .055));
+            double taxRate = 0;
+            // Find the tax rate for the user's state of residence. Defaults to NE
+            switch (state)
+            {
+                case "AL":
+                    taxRate = .04;
+                    break;
+                case "AK":
+                    taxRate = .00;
+                    break;
+                case "AZ":
+                    taxRate = .056;
+                    break;
+                case "AR":
+                    taxRate = .065;
+                    break;
+                case "CA":
+                    taxRate = .0725;
+                    break;
+                case "CO":
+                    taxRate = .029;
+                    break;
+                case "CT":
+                    taxRate = .0635;
+                    break;
+                case "DE":
+                    taxRate = .00;
+                    break;
+                case "FL":
+                    taxRate = .06;
+                    break;
+                case "GA":
+                    taxRate = .04;
+                    break;
+                case "HI":
+                    taxRate = .04;
+                    break;
+                case "ID":
+                    taxRate = .06;
+                    break;
+                case "IL":
+                    taxRate = .0625;
+                    break;
+                case "IN":
+                    taxRate = .07;
+                    break;
+                case "IA":
+                    taxRate = .06;
+                    break;
+                case "KS":
+                    taxRate = .065;
+                    break;
+                case "KY":
+                    taxRate = .06;
+                    break;
+                case "LA":
+                    taxRate = .0445;
+                    break;
+                case "ME":
+                    taxRate = .055;
+                    break;
+                case "MD":
+                    taxRate = .06;
+                    break;
+                case "MA":
+                    taxRate = .0625;
+                    break;
+                case "MI":
+                    taxRate = .06;
+                    break;
+                case "MN":
+                    taxRate = .0688;
+                    break;
+                case "MS":
+                    taxRate = .07;
+                    break;
+                case "MO":
+                    taxRate = .0423;
+                    break;
+                case "MT":
+                    taxRate = .00;
+                    break;
+                case "NE":
+                    taxRate = .055;
+                    break;
+                case "NV":
+                    taxRate = .0685;
+                    break;
+                case "NH":
+                    taxRate = .00;
+                    break;
+                case "NJ":
+                    taxRate = .0663;
+                    break;
+                case "NM":
+                    taxRate = .0488;
+                    break;
+                case "NY":
+                    taxRate = .04;
+                    break;
+                case "NC":
+                    taxRate = .0475;
+                    break;
+                case "ND":
+                    taxRate = .05;
+                    break;
+                case "OH":
+                    taxRate = .0575;
+                    break;
+                case "OK":
+                    taxRate = .045;
+                    break;
+                case "OR":
+                    taxRate = .00;
+                    break;
+                case "PA":
+                    taxRate = .06;
+                    break;
+                case "RI":
+                    taxRate = .07;
+                    break;
+                case "SC":
+                    taxRate = .06;
+                    break;
+                case "SD":
+                    taxRate = .042;
+                    break;
+                case "TN":
+                    taxRate = .07;
+                    break;
+                case "TX":
+                    taxRate = .0625;
+                    break;
+                case "UT":
+                    taxRate = .061;
+                    break;
+                case "VT":
+                    taxRate = .06;
+                    break;
+                case "VA":
+                    taxRate = .053;
+                    break;
+                case "WA":
+                    taxRate = .065;
+                    break;
+                case "WV":
+                    taxRate = .06;
+                    break;
+                case "WI":
+                    taxRate = .05;
+                    break;
+                case "WY":
+                    taxRate = .04;
+                    break;
+                case "DC":
+                    taxRate = .06;
+                    break;
+                default:
+                    taxRate = .055;
+                    break;
+            }
+
+            return Math.Round(gross + (gross * taxRate), 2);
         }
     }
 }
--- Engines/ICartEngine.cs
+++ Engines/ICartEngine.cs
@@ -8,13 +8,13 @@ namespace Engines
 {
     internal interface ICartEngine
     {
-        // Allows printing a single item's cost for receipt
-        double calculateItemCost(float salePct, int units, double pricePerUnit);
+        // Allows printing a single item's cost to two decimal places for receipt
+        double calculateItemCost(double salePct, int units, double pricePerUnit);
 
         // Allows printing the taxless total for receipt
         double calculateTotal(List<double> itemCosts);
 
-        // Generates the total
-        double totalWithTax(double gross);
+        // Generates the total to two decimal places
+        double totalWithTax(double gross, string state);
     }
 }
