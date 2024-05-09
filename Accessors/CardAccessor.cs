c476385c4763a82f824369d2c15f1a85dca43ea7
Michael K West <mwest35@huskers.unl.edu>
2024-05-09 17:19:52 -0500

    spread out code in card controller among managers and accessors

---
+++ Accessors/CardAccessor.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Data.SqlClient;
+
+namespace Accessors
+{
+    public class CardAccessor : ICardAccessor
+    {
+        SqlConnection conn =
+            new SqlConnection("data source = Kalelius\\SQLEXPRESS; initial catalog = grocery; TrustServerCertificate = True; user id = sa; password = sixpeasinapod");
+
+        List<string> ICardAccessor.RetrieveCardInfo(int userId)
+        {
+            string name = "";
+            string cardNumber = "";
+            string cvv = "";
+            string date = "";
+            List<string> card = new List<string>();
+            string query = "select name, cardNumber, cvv, expDate from Card where card_id = @userId";
+            using (SqlCommand cmd = new SqlCommand(query))
+            {
+                cmd.Parameters.Add("@userId", System.Data.SqlDbType.Int);
+                cmd.Parameters["@userId"].Value = userId;
+                cmd.Connection = conn;
+
+                try
+                {
+                    cmd.Connection.Open();
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            name = reader.GetString(0);
+                            cardNumber = reader.GetString(1);
+                            cvv = reader.GetString(2);
+                            date = reader.GetString(3);
+                        }
+                    }
+                    cmd.Connection.Close();
+                }
+                catch (SqlException exception)
+                {
+                    throw new Exception(exception.Message);
+                }
+            }
+            card.Add(name);
+            card.Add(cardNumber);
+            card.Add(cvv);
+            card.Add(date);
+            return card;
+        }
+
+        void ICardAccessor.UpdateCardInfo(int userId, string name, string num, string cvv, string date_month, string date_year)
+        {
+            string query = "update Card set name = @name, cardNumber = @num, cvv = @cvv, expDate = @date where card_id = @userId";
+            using (SqlCommand cmd = new SqlCommand(query))
+            {
+                cmd.Parameters.Add("@name", System.Data.SqlDbType.NVarChar, 50);
+                cmd.Parameters.Add("@num", System.Data.SqlDbType.NVarChar, 19);
+                cmd.Parameters.Add("@cvv", System.Data.SqlDbType.NVarChar, 3);
+                cmd.Parameters.Add("@date", System.Data.SqlDbType.NVarChar, 5);
+                cmd.Parameters.Add("@id", System.Data.SqlDbType.Int);
+
+                cmd.Parameters["@name"].Value = name;
+                cmd.Parameters["@num"].Value = num;
+                cmd.Parameters["@cvv"].Value = cvv;
+                cmd.Parameters["@date"].Value = $"{date_month}/{date_year}";
+                cmd.Parameters["@userId"].Value = userId;
+                cmd.Connection = conn;
+
+                try
+                {
+                    cmd.Connection.Open();
+                    cmd.ExecuteNonQuery();
+                    cmd.Connection.Close();
+                }
+                catch (SqlException exception)
+                {
+                    throw new Exception(exception.Message);
+                }
+            }
+
+            return;
+        }
+    }
+}
---
+++ Accessors/ICardAccessor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Accessors
+{
+    public interface ICardAccessor
+    {
+        List<String> RetrieveCardInfo(int userId);
+
+        void UpdateCardInfo(int userId, string name, string num, string cvv, string date_month, string date_year);
+    }
+}
